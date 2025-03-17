350412656215cf9029e9f09d973ea80106d75d61
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-03-17 12:35:59 +0000

    add manager details service

--- src/main/java/com/example/medjool/services/ManagerDetailService.java
+++ src/main/java/com/example/medjool/services/ManagerDetailService.java
@@ -1,2 +1,20 @@
-package com.example.medjool.services;public class ManagerDetailService {
+package com.example.medjool.services;
+
+import com.example.medjool.repository.ManageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ManagerDetailService implements UserDetailsService {
+    private final ManageRepository manageRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return manageRepository.findByEmail(username);
+    }
 }
