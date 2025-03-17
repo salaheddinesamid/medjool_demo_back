2029bce2452c9f11edf10130a6b6c1af8264b05c
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-03-17 11:32:38 +0000

    add jwt filter

--- src/main/java/com/example/medjool/security/JwtFilterChain.java
+++ src/main/java/com/example/medjool/security/JwtFilterChain.java
@@ -1,2 +1,27 @@
-package com.example.medjool.security;public class JwtFilterChain {
+package com.example.medjool.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilterChain extends OncePerRequestFilter {
+
+    private final JwtUtilities jwtUtilities;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = jwtUtilities.getToken(request);
+
+    }
 }
