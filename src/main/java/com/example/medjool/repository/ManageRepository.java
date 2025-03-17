12fa7d98702e3e3b81f8d4feb90437504d5c8ebe
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-03-17 11:34:35 +0000

    manager repository

--- src/main/java/com/example/medjool/repository/ManageRepository.java
+++ src/main/java/com/example/medjool/repository/ManageRepository.java
@@ -1,2 +1,9 @@
-package com.example.medjool.repository;public interface ManageRepository {
+package com.example.medjool.repository;
+
+import com.example.medjool.model.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManageRepository extends JpaRepository<Manager,Integer> {
+
+    Manager findByEmail(String email);
 }
