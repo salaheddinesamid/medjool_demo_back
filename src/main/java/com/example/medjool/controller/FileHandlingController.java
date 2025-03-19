d46ae6143024a24ce48a991d177ea026c4c47cf4
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-03-19 10:09:00 +0000

    add file handling

--- src/main/java/com/example/medjool/controller/FileHandlingController.java
+++ src/main/java/com/example/medjool/controller/FileHandlingController.java
@@ -1,18 +1,32 @@
 package com.example.medjool.controller;
 
 import com.example.medjool.services.FileHandling;
+import com.example.medjool.services.ParseCSV;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api/file/")
 public class FileHandlingController {
     private final FileHandling fileHandling;
+    private final ParseCSV parseCSV;
 
-    public FileHandlingController(FileHandling fileHandling) {
+    public FileHandlingController(FileHandling fileHandling, ParseCSV parseCSV) {
         this.fileHandling = fileHandling;
+        this.parseCSV = parseCSV;
+    }
+
+
+    @PostMapping(value = "stock/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> updateStock(@RequestParam("file")MultipartFile multipartFile){
+
+        if (multipartFile.isEmpty()) {
+            return ResponseEntity.badRequest().body("Please upload a CSV file.");
+        }
+
+        return parseCSV.parseCSV(multipartFile);
     }
 
 }
