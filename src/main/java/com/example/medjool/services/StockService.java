b565f7b0d7e00ed5232acb4ad718cde3e6620735
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-03-19 21:09:48 +0000

    change the stock service, update the stock with CSV

--- src/main/java/com/example/medjool/services/StockService.java
+++ src/main/java/com/example/medjool/services/StockService.java
@@ -2,9 +2,18 @@ package com.example.medjool.services;
 
 import com.example.medjool.model.Product;
 import com.example.medjool.repository.ProductRepository;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @Service
@@ -24,5 +33,22 @@ public class StockService {
 
     public List<Product> getAllProducts(){
         return productRepository.findAll();
+    }
+
+
+    // Upload a CSV file to update the stock
+    public ResponseEntity<Object> updateStock(MultipartFile file) throws IOException {
+
+        // Create a buffered reader:
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        // Parse the csv:
+        CSVParser csvParser = new CSVParser(bufferedReader, CSVFormat.DEFAULT);
+
+        for (CSVRecord record : csvParser) {
+            Product product = productRepository.findById(Integer.valueOf(record.get("product_id"))).get();
+            product.setTotalWeight(Float.valueOf(record.get("New weight")));
+            productRepository.save(product);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
