baca94720cf75dd38b1de11a21cc5f1be7574ff7
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-03-21 09:35:50 +0000

    update client service, and add unit testing for adding new client

--- src/main/java/com/example/medjool/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.medjool.repository;
-
-import com.example.medjool.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
