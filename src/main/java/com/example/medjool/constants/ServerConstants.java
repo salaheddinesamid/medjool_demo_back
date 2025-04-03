6970682d85cf5a9199249bb58efe1607e728a5fa
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-03 16:21:57 +0000

    adding server constants

--- src/main/java/com/example/medjool/constants/ServerConstants.java
+++ src/main/java/com/example/medjool/constants/ServerConstants.java
@@ -1,4 +1,11 @@
 package com.example.medjool.constants;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class ServerConstants {
+
+    private static final String LOW_STOCK = "PAY ATTENTION, THIS PRODUCT IS LOW STOCK";
 }
