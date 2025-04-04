39af1f7e5e230ebedaa44e7bc7d7bfdab334f837
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-04 16:25:49 +0000

    find producy by callibre, color and quality

--- src/main/java/com/example/medjool/repository/ProductRepository.java
+++ src/main/java/com/example/medjool/repository/ProductRepository.java
@@ -4,5 +4,5 @@ import com.example.medjool.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByCallibreAndQuality(String callibre, String quality);
+    Product findByCallibreAndColorAndQuality(String callibre, String color, String quality);
 }
