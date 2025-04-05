8bbe05871b0f18ce6df134936a244adf78952b2f
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-05 09:47:03 +0000

    adding order exception controller

--- src/main/java/com/example/medjool/exception/OrderExceptionController.java
+++ src/main/java/com/example/medjool/exception/OrderExceptionController.java
@@ -1,4 +1,16 @@
 package com.example.medjool.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class OrderExceptionController {
+
+
+    @ExceptionHandler(OrderCannotBeCanceledException.class)
+    public ResponseEntity<Object> handleOrderCannotBeCanceledException(OrderCannotBeCanceledException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
