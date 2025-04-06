97b87a5d5ab02e3b6807e6729424efda0ab82888
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-06 14:58:44 +0100

    update and fix bugs

--- src/main/java/com/example/medjool/repository/OrderDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.medjool.repository;
-
-import com.example.medjool.documents.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OrderDocumentRepository extends MongoRepository<Order,String> {
-}
