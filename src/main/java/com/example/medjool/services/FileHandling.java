060700e9501ed5dd529e2494b0cb7eab62a285d1
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-06 15:20:14 +0100

    handle order PDF upload

--- src/main/java/com/example/medjool/services/FileHandling.java
+++ src/main/java/com/example/medjool/services/FileHandling.java
@@ -1,16 +1,10 @@
 package com.example.medjool.services;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FileHandling {
 
-    // upload file
-    /*
-    public ResponseEntity<Object> downloadOrderPDF(Integer id){
 
-    }
-     */
 
 }
