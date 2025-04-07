2d82694cc495bee4d88bb2958864fcaf08665032
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-07 10:26:23 +0100

    implement overview service interface

--- src/main/java/com/example/medjool/services/implementation/OverviewServiceImpl.java
+++ src/main/java/com/example/medjool/services/implementation/OverviewServiceImpl.java
@@ -1,4 +1,61 @@
 package com.example.medjool.services.implementation;
 
-public class OverviewServiceImpl {
+import com.example.medjool.constants.ServerConstants;
+import com.example.medjool.dto.OverviewDto;
+import com.example.medjool.model.Order;
+import com.example.medjool.model.OrderStatus;
+import com.example.medjool.model.Product;
+import com.example.medjool.repository.OrderRepository;
+import com.example.medjool.repository.ProductRepository;
+import com.example.medjool.services.OverviewService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OverviewServiceImpl implements OverviewService {
+
+    private final ProductRepository productRepository;;
+    private final OrderRepository orderRepository;
+
+    private final AlertServiceImpl alertService;
+    private final ServerConstants serverConstants;
+
+    public OverviewServiceImpl(ProductRepository productRepository, OrderRepository orderRepository, AlertServiceImpl alertService, ServerConstants serverConstants) {
+        this.productRepository = productRepository;
+        this.orderRepository = orderRepository;
+        this.alertService = alertService;
+        this.serverConstants = serverConstants;
+    }
+
+
+    @Override
+    public ResponseEntity<OverviewDto> getOverview() {
+        OverviewDto overviewDto = new OverviewDto();
+
+        Double totalStockWeight = 0d;
+
+        float total_revenue = 0f;
+        for (Product product : productRepository.findAll()) {
+
+            if (product.getTotalWeight() <= 200){
+                String content = "The product id: " + product.getProductId() + "is low stock";
+                alertService.newAlert(content);
+            }
+            totalStockWeight += product.getTotalWeight();
+        }
+
+        for (Order order : orderRepository.findAll()) {
+            total_revenue += (float) order.getTotalPrice();
+        }
+        Long totalOrders = orderRepository.count();
+        Long totalReceivedOrders = (long) orderRepository.findAllByStatus(OrderStatus.valueOf("RECEIVED")).size();
+
+        overviewDto.setTotalStock(totalStockWeight);
+        overviewDto.setTotalOrders(totalOrders);
+        overviewDto.setTotalReceivedOrders(totalReceivedOrders);
+        overviewDto.setTotalRevenue(total_revenue);
+
+        return new ResponseEntity<>(overviewDto, HttpStatus.OK);
+    }
 }
