5ba4885f6ba0450af37f9e382aae93b247a332c6
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-07 10:23:28 +0100

    update packaging data type from str to float

--- src/main/java/com/example/medjool/dto/PalletDto.java
+++ src/main/java/com/example/medjool/dto/PalletDto.java
@@ -1,12 +1,11 @@
 package com.example.medjool.dto;
 
-import jakarta.persistence.Column;
 import lombok.Data;
 
 @Data
 public class PalletDto {
 
-    String packaging;
+    float  packaging;
     Integer numberOfBoxesInCarton;
     Integer numberOfCartonsInStory;
     Integer numberOfStoriesInPallet;
