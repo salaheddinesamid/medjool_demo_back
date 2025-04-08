4c0ce16012cc6aec741237c73869d107c696213a
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-08 17:41:27 +0100

    add constructures

--- src/main/java/com/example/medjool/dto/OrderRequestDto.java
+++ src/main/java/com/example/medjool/dto/OrderRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.medjool.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class OrderRequestDto {
     private String clientName;
     private List<OrderItemRequestDto> items;
