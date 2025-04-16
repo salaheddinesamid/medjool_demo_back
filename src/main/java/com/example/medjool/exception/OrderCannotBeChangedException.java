1f6dece43f5c90229750831926fb6fcab07a95d6
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-16 16:12:17 +0100

    add new order exception controller' '

--- src/main/java/com/example/medjool/exception/OrderCannotBeChangedException.java
+++ src/main/java/com/example/medjool/exception/OrderCannotBeChangedException.java
@@ -1,7 +1,6 @@
 package com.example.medjool.exception;
 
 public class OrderCannotBeChangedException extends RuntimeException {
-  public OrderCannotBeChangedException(String message) {
-    super(message);
-  }
+
+
 }
--- src/main/java/com/example/medjool/exception/OrderExceptionController.java
+++ src/main/java/com/example/medjool/exception/OrderExceptionController.java
@@ -18,4 +18,9 @@ public class OrderExceptionController {
     public ResponseEntity<Object> handleOrderNotFoundException(OrderNotFoundException ex) {
         return new ResponseEntity<>("Order not found... Please, select an appropriate order to move forward.", HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(OrderCannotBeChangedException.class)
+    public ResponseEntity<Object> handleOrderChangeException(OrderCannotBeChangedException ex) {
+        return new ResponseEntity<>("The order is READY TO SHIP. It can not be changed or canceled...", HttpStatus.BAD_REQUEST);
+    }
 }
