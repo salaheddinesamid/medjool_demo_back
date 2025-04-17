3efe6c1aee472f067c70e98ca3af4349b4e66d2d
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-17 10:51:09 +0100

    jwt utilities

--- src/main/java/com/example/medjool/jwt/JwtUtilities.java
+++ src/main/java/com/example/medjool/jwt/JwtUtilities.java
@@ -1,4 +1,23 @@
 package com.example.medjool.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class JwtUtilities {
+
+    private static String SECRET_KEY = "65A1716B65367C69B4BDCC822A44A";
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
 }
