462b62a5ad33a372ea22caa0f1537c1f2e9287da
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-17 10:48:49 +0100

    add a security configuration

--- src/main/java/com/example/medjool/config/SecurityConfiguration.java
+++ src/main/java/com/example/medjool/config/SecurityConfiguration.java
@@ -1,4 +1,31 @@
 package com.example.medjool.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return
+                http.csrf()
+                        .disable()
+                        .authorizeHttpRequests(authorizeRequests ->
+                                authorizeRequests.requestMatchers("/api/order/**").permitAll())
+                        .build();
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
