f1de64c5958eaa9801e92e92d6c1b5c84ae955d4
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-18 11:15:17 +0100

    update user details response

--- src/main/java/com/example/medjool/dto/UserDetailsDto.java
+++ src/main/java/com/example/medjool/dto/UserDetailsDto.java
@@ -1,4 +1,16 @@
 package com.example.medjool.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
 public class UserDetailsDto {
+    private long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
+    private boolean active;
 }
