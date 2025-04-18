6f663bf06ac52827198227bdf4d4b1c8b1c4bd2d
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-18 11:13:03 +0100

    add user management feature

--- src/main/java/com/example/medjool/repository/NotificationRepository.java
+++ src/main/java/com/example/medjool/repository/NotificationRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> findAllByRead(boolean read);
-    Notification findByContent(String productId);
+    boolean existsByContent(String content);
 }
