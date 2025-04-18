92ddc763f6d612f751a2cc2bccd873c0f5ec23d3
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-18 11:53:54 +0100

    handling exceptions fot locked accounts that needs confirmation

--- src/main/java/com/example/medjool/exception/UserAccountLockedException.java
+++ src/main/java/com/example/medjool/exception/UserAccountLockedException.java
@@ -1,7 +1,5 @@
 package com.example.medjool.exception;
 
 public class UserAccountLockedException extends RuntimeException {
-  public UserAccountLockedException(String message) {
-    super(message);
-  }
+
 }
