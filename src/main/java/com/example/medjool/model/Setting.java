e3afa2db8472d7cd195f32956c0383804a944d47
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-18 17:31:42 +0100

    add setting entity

--- src/main/java/com/example/medjool/model/Setting.java
+++ src/main/java/com/example/medjool/model/Setting.java
@@ -1,4 +1,32 @@
 package com.example.medjool.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "settings")
 public class Setting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long settingId;
+
+    @Column(name = "attribute_name")
+    @Enumerated(EnumType.STRING)
+    private SystemSettings attributeName;
+
+    @Column(name = "attribute_value")
+    private String attributeValue;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "latest_update")
+    private LocalDateTime latestUpdate;
+
 }
