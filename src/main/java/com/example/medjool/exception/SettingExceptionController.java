6037f01884d49fab2c165b969aae30eec05f43de
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-21 10:53:56 +0100

    add setting exception controller

--- src/main/java/com/example/medjool/exception/SettingExceptionController.java
+++ src/main/java/com/example/medjool/exception/SettingExceptionController.java
@@ -1,4 +1,14 @@
 package com.example.medjool.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class SettingExceptionController {
+    @ExceptionHandler(SettingNotFoundException.class)
+    public ResponseEntity<Object> handleSettingNotFoundException(SettingNotFoundException e) {
+        return new ResponseEntity<>("Setting not found", HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/example/medjool/exception/SettingNotFoundException.java
+++ src/main/java/com/example/medjool/exception/SettingNotFoundException.java
@@ -1,7 +1,5 @@
 package com.example.medjool.exception;
 
 public class SettingNotFoundException extends RuntimeException {
-  public SettingNotFoundException(String message) {
-    super(message);
-  }
+
 }
