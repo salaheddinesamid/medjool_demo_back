0a18f4300f04bbed2510379b153f14b5231c6699
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-21 10:53:06 +0100

    add setting model

--- src/main/java/com/example/medjool/model/SystemSetting.java
+++ src/main/java/com/example/medjool/model/SystemSetting.java
@@ -1,4 +1,23 @@
 package com.example.medjool.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "system_setting")
 public class SystemSetting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "config_key") // renamed to avoid reserved keyword issue
+    private String key;
+
+    @Column(name = "value")
+    private double value;
+
 }
--- src/main/java/com/example/medjool/repository/SystemSettingRepository.java
+++ src/main/java/com/example/medjool/repository/SystemSettingRepository.java
@@ -1,4 +1,8 @@
 package com.example.medjool.repository;
 
-public interface SystemSettingRepository {
+import com.example.medjool.model.SystemSetting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SystemSettingRepository extends JpaRepository<SystemSetting, Long> {
+    SystemSetting findByKey(String key);
 }
