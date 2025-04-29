d2c1875b042f623e8193957fa03a891dcd443727
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-04-29 09:58:53 +0100

    add a role initializer

--- src/main/java/com/example/medjool/component/RoleInitializer.java
+++ src/main/java/com/example/medjool/component/RoleInitializer.java
@@ -1,4 +1,40 @@
 package com.example.medjool.component;
 
+import com.example.medjool.model.Role;
+import com.example.medjool.model.RoleName;
+import com.example.medjool.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RoleInitializer {
+
+    private final RoleRepository roleRepository;
+    private static final RoleName[] roles = {
+            RoleName.GENERAL_MANAGER,
+            RoleName.SALES,
+            RoleName.LOGISTICS,
+            RoleName.FINANCE,
+            RoleName.FACTORY
+    };
+    @Autowired
+    public RoleInitializer(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initialize() {
+        for (RoleName roleName : roles) {
+            if (!roleRepository.existsByRoleName(roleName)) {
+                Role role = new Role();
+                // NO manual ID setting here
+                role.setRoleName(roleName);
+                roleRepository.save(role);
+            }
+        }
+
+    }
+
 }
