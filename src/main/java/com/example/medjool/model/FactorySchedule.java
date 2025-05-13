30ef9c629e92f3e1ebf3aaa09ee5acca604950ce
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-05-13 15:39:59 +0100

    add a table that stores factory daily schedules

--- src/main/java/com/example/medjool/model/FactorySchedule.java
+++ src/main/java/com/example/medjool/model/FactorySchedule.java
@@ -1,4 +1,33 @@
 package com.example.medjool.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class FactorySchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleId;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(name = "working_hours")
+    private Integer workingHours;
+
+    @Column(name = "remaining_hours")
+    private Integer remainingHours;
+
+    @Column(name = "is_available")
+    private Boolean isAvailable;
+
+    @OneToMany
+    List<Order> orders;
 }
