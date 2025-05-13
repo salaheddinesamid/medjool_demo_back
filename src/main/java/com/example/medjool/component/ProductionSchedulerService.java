930e548ce84f8e5cadda9e9b92e787e449840c27
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-05-13 16:55:25 +0100

    schedule production orders

--- src/main/java/com/example/medjool/component/ProductionSchedulerService.java
+++ src/main/java/com/example/medjool/component/ProductionSchedulerService.java
@@ -1,17 +1,17 @@
 package com.example.medjool.component;
 
-import com.example.medjool.model.ProductionOrder;
-import com.example.medjool.model.ProductionStatus;
-import com.example.medjool.model.SystemSetting;
-import com.example.medjool.repository.OrderHistoryRepository;
-import com.example.medjool.repository.OrderRepository;
-import com.example.medjool.repository.ProductionOrderRepository;
-import com.example.medjool.repository.SystemSettingRepository;
+import com.example.medjool.model.*;
+import com.example.medjool.repository.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,22 +22,68 @@ public class ProductionSchedulerService {
     private final OrderRepository orderRepository;
     private final ProductionOrderRepository productionOrderRepository;
     private final SystemSettingRepository systemSettingRepository;
+    private final FactoryScheduleRepository factoryScheduleRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(ProductionSchedulerService.class);
 
     @Autowired
-    public ProductionSchedulerService(OrderHistoryRepository orderHistoryRepository, OrderRepository orderRepository, ProductionOrderRepository productionOrderRepository, SystemSettingRepository systemSettingRepository) {
+    public ProductionSchedulerService(OrderHistoryRepository orderHistoryRepository, OrderRepository orderRepository, ProductionOrderRepository productionOrderRepository, SystemSettingRepository systemSettingRepository, FactoryScheduleRepository factoryScheduleRepository) {
         this.orderHistoryRepository = orderHistoryRepository;
         this.orderRepository = orderRepository;
         this.productionOrderRepository = productionOrderRepository;
         this.systemSettingRepository = systemSettingRepository;
+        this.factoryScheduleRepository = factoryScheduleRepository;
     }
 
     @Transactional
-    @Scheduled()
+    @Scheduled(fixedRate = 12 * 60 * 60 * 1000) // Runs every half a day
     public void scheduleProductionOrders() {
         List<ProductionOrder> productionOrderInProgress = productionOrderRepository.findAllByProductionStatus(ProductionStatus.IN_PROGRESS);
         List<ProductionOrder> productionOrderNotStarted  = productionOrderRepository.findAllByProductionStatus(ProductionStatus.NOT_STARTED);
-        SystemSetting systemSetting = systemSettingRepository.findByKey("factory_working_hours").get();
+        double factoryWorkingHours = systemSettingRepository.findByKey("factory_working_hours").get().getValue();
+
+        List<Order> scheduledOrders = new ArrayList<>(); // Initialize the list of orders to be scheduled...
+        FactorySchedule factorySchedule = new FactorySchedule(); // Create new factory schedule
+        factorySchedule.setDateTime(LocalDateTime.now());
+        factorySchedule.setWorkingHours(factoryWorkingHours);
 
 
+        // Scan the orders in progress first:
+        if(productionOrderInProgress.isEmpty() && !productionOrderNotStarted.isEmpty()) {
+            double workingHoursRate = factoryWorkingHours / productionOrderNotStarted.size();
+            for(ProductionOrder productionOrder : productionOrderNotStarted) {
+                Order order = productionOrder.getOrder();
+                productionOrder.setRemainingHours(productionOrder.getRemainingHours() - workingHoursRate);
+                scheduledOrders.add(order);
+            }
+        }
+
+        // When there are no orders in progress and there are orders not started yet:
+
+        else if(!productionOrderInProgress.isEmpty() && productionOrderNotStarted.isEmpty()){
+            double workingHoursRate = factoryWorkingHours / productionOrderInProgress.size();
+            for(ProductionOrder productionOrder : productionOrderInProgress) {
+                Order order = productionOrder.getOrder();
+                productionOrder.setRemainingHours(productionOrder.getRemainingHours() - workingHoursRate);
+                factorySchedule.setRemainingHours(factorySchedule.getRemainingHours() - workingHoursRate);
+                scheduledOrders.add(order);
+            }
+        }
+
+        // When there are orders in progress and orders not started yet:
+        else if(!productionOrderInProgress.isEmpty() && !productionOrderNotStarted.isEmpty()){
+            double workingHoursRate = factoryWorkingHours / (productionOrderInProgress.size() + productionOrderNotStarted.size());
+            for(ProductionOrder productionOrder : productionOrderInProgress) {
+                Order order = productionOrder.getOrder();
+                productionOrder.setRemainingHours(productionOrder.getRemainingHours() - workingHoursRate);
+                factorySchedule.setRemainingHours(factorySchedule.getRemainingHours() - workingHoursRate);
+                scheduledOrders.add(order);
+            }
+            for(ProductionOrder productionOrder : productionOrderNotStarted) {
+                Order order = productionOrder.getOrder();
+                productionOrder.setRemainingHours(productionOrder.getRemainingHours() - workingHoursRate);
+                scheduledOrders.add(order);
+            }
+        }
     }
 }
