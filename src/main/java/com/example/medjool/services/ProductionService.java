2da8a777d17e06c2974a4844926cc4daaacc3352
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2025-05-13 15:37:52 +0100

    add method that push orders in production

--- src/main/java/com/example/medjool/services/ProductionService.java
+++ src/main/java/com/example/medjool/services/ProductionService.java
@@ -1,4 +1,13 @@
 package com.example.medjool.services;
 
+import com.example.medjool.dto.ProductionDetailsResponseDto;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ProductionService {
+
+    void pushIntoProduction(Long orderId, LocalDateTime startDate) throws Exception;
+    ResponseEntity<List<ProductionDetailsResponseDto>> getAllProductionDetails() throws Exception;
 }
--- src/main/java/com/example/medjool/services/implementation/ProductionServiceImpl.java
+++ src/main/java/com/example/medjool/services/implementation/ProductionServiceImpl.java
@@ -1,4 +1,58 @@
 package com.example.medjool.services.implementation;
 
-public class ProductionServiceImpl {
+import com.example.medjool.dto.ProductionDetailsResponseDto;
+import com.example.medjool.model.Order;
+import com.example.medjool.model.ProductionOrder;
+import com.example.medjool.model.ProductionStatus;
+import com.example.medjool.repository.FactoryScheduleRepository;
+import com.example.medjool.repository.OrderRepository;
+import com.example.medjool.repository.ProductionOrderRepository;
+import com.example.medjool.services.ProductionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ProductionServiceImpl implements ProductionService {
+
+    private final ProductionOrderRepository productionOrderRepository;
+    private final OrderRepository orderRepository;
+    private final FactoryScheduleRepository factoryScheduleRepository;
+
+    @Autowired
+    public ProductionServiceImpl(ProductionOrderRepository productionOrderRepository, OrderRepository orderRepository, FactoryScheduleRepository factoryScheduleRepository) {
+        this.productionOrderRepository = productionOrderRepository;
+        this.orderRepository = orderRepository;
+        this.factoryScheduleRepository = factoryScheduleRepository;
+    }
+
+    @Override
+    public void pushIntoProduction(Long orderId, LocalDateTime startDate) throws Exception {
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new Exception("Order not found"));
+        if (productionOrderRepository.existsById(orderId)) {
+            throw new Exception("Order already in production");
+        }
+
+        ProductionOrder productionOrder = new ProductionOrder();
+        productionOrder.setStartDate(startDate);
+        productionOrder.setOrder(order);
+        productionOrder.setWorkingHours(order.getWorkingHours());
+        productionOrder.setRemainingHours(order.getWorkingHours());
+        productionOrder.setProductionStatus(ProductionStatus.NOT_STARTED);
+        System.out.println("Pushing order " + orderId + " into production starting from " + startDate);
+
+        productionOrderRepository.save(productionOrder);
+    }
+
+    @Override
+    public ResponseEntity<List<ProductionDetailsResponseDto>> getAllProductionDetails() throws Exception {
+        List<ProductionOrder> productionOrders = productionOrderRepository.findAll();
+        return ResponseEntity.ok(productionOrders.stream()
+                .map(ProductionDetailsResponseDto::new).toList());
+    }
 }
